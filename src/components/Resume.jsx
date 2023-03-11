92a013a0a9ab371034a97932a0b74fc28e6c412d
ArekBM <eric.bmiyake@gmail.com>
2023-03-10 17:14:41 -0800

    trying to fix resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,15 +4,25 @@ import ParticleEngine from './ParticleEngine';
 import Button from 'react-bootstrap/Button';
 import { AiOutlineDownload } from 'react-icons/ai';
 import pdf from '../assets/Resume2023.pdf';
-import { Document, Page, pdfjs } from 'react-pdf';
+import { Document, Page } from 'react-pdf';
 
 export default function Resume() {
+
+    const url = 'https://drive.google.com/file/d/1Zu1XjZ-PRz9qI_ii7A_cedx5zmAwW8DV/view?usp=share_link'
+
     const [width, setWidth] = useState(1200);
+    const [numPages, setNumPages ] = useState(null)
+    const [pageNumber, setPageNumber] = useState(1)
+
 
     useEffect(() => {
         setWidth(window.innerWidth);
     }, []);
-    
+
+    function onDocumentSuccess({numPages}){
+        setNumPages(numPages)
+    }
+
     return(
         <div>
             <Container fluid className='resume-section'>
@@ -29,8 +39,8 @@ export default function Resume() {
                     </Button>
                 </Row>
                 <Row className='resume'>
-                    <Document file={pdf} className='d-flex justify-content-center'>
-                        <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+                    <Document file={pdf} className='d-flex justify-content-center' onLoadSucess={onDocumentSuccess}>
+                        <Page pageNumber={pageNumber} scale={width > 786 ? 1.7 : 0.6} />
                     </Document>
                 </Row>
             </Container>

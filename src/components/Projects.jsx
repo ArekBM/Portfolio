53af84ebed741530855846e85989da93c2b5de9c
ArekBM <eric.bmiyake@gmail.com>
2023-03-14 16:10:51 -0700

    added Kitchen Sink

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,6 +4,7 @@ import ProjectCard from './ProjectCard';
 import BuildA from '../assets/BuildA.png';
 import ParticleEngine from './ParticleEngine';
 import PhotoCapsule from '../assets/PhotoCapsule.png';
+import KitchenSink from '../assets/KitchenSink.jpg'
 
 export default function Projects() {
     return(
@@ -30,6 +31,14 @@ export default function Projects() {
                             description='A game where you are presented with a series of intriguing photos from different eras and your goal is to guess which year it was taken. Written with React and Javascript. I setup an API with MongoDB which will insert and store scores and a leaderboard that calls and displays the top 10 of the day.'
                             ghLink='https://github.com/ArekBM/PhotoCapsule'
                             demoLink='https://scintillating-melba-98a2d4.netlify.app/'
+                        />
+                    </Col>
+                    <Col md={4} className='project-card'>
+                        <ProjectCard
+                            imgPath={KitchenSink}
+                            title='KitchenSink'
+                            description='A social network site built in Django which supports creating users, posts, likes, unlikes and edits. Built with python, Bootstrap, vanilla Javascript and HTML.'
+                            ghLink='https://github.com/ArekBM/KitchenSink'
                         />
                     </Col>
                 </Row>
